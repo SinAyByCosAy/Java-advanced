93cc52ae2cb070a2b450ff5a39b85bb3ec583986
tanaysri98 <tanay.srivastava2016@vitstudent.ac.in>
2020-05-05 00:50:03 +0530

    Wikepida Downloader Mini Project

--- TaskManager.java
+++ TaskManager.java
@@ -3,9 +3,7 @@ package tech.codingclub;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
-/**
- * Created by Coding Club India.
- */
+
 public class TaskManager {
 
     private int threadCount;
